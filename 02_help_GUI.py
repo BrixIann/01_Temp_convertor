19a0ee5a5f3eb76e40ebe8b9c49de6bca7e1a7a1
BrixIann <78567267+BrixIann@users.noreply.github.com>
2021-03-22 09:22:56 +1300

    Create 02_help_GUI

--- 02_help_GUI.py
+++ 02_help_GUI.py
@@ -2,7 +2,7 @@ from tkinter import *
 import random
 
 
-class Foo:
+class Convertor:
     def __init__(self, parent):
         print("hello world")
 
@@ -10,6 +10,6 @@ class Foo:
 # main routine
 if __name__ == "__main__":
     root = Tk()
-    root.title("title goes here")
-    something = Foo(root)
+    root.title("Temperature Convertor")
+    something = Convertor(root)
     root.mainloop()
