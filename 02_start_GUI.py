bed37817e61eb04c761d9678858fc9be19900de2
BrixIann <78567267+BrixIann@users.noreply.github.com>
2021-08-11 12:56:30 +1200

    start

--- 02_start_GUI.py
+++ 02_start_GUI.py
@@ -13,7 +13,31 @@ class Start:
         self.mystery_box_label = Label(self.start_frame, text="Mystery Box Game", font="Arial 19 bold")
         self.mystery_box_label.grid(row=1)
 
+        #Initial Instructions (row 1)
+        self.mystery_instructions = Label(self.start_frame, font="Arial 10 italic", text="Please enter a dollar amount"
+                                                                                         "(between $5 and $50) in the box"
+                                                                                         "below. Then choose the"  
+                                                                                         "stakes. The higher the stakes,"
+                                                                                         "the more you can win!", wrap=275, justify=LEFT, padx=10, pady=10)
+                                                                                         
+
         #Entry box... (row 1)
         self.start_amount_entry = Entry(self.start_frame, font="Arial 16 bold")
         self.start_amount_entry.grid(row=2)
         
+
+        #Play Button (row 2)
+        self.lowstakes_button = Button(text="Low ($5)", command=lambda: self.to_game(1))
+        self.lowstakes_button.grid(row=2, pady=10)
+
+    def to_game(self, stakes):
+        starting_balance = self.start_amount_entry.get()
+        Game(self, stakes, starting_balance)
+
+
+class Game:
+    def __init__(self, partner, stakes, starting_balance):
+        print(stakes)
+        print(starting_balance)
+
+
